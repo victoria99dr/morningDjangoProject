1c8777e4e5466683ea494c36f1ce3cc71228e119
victoria99dr <baritori27@gmail.com>
2023-03-06 10:22:20 +0300

    added login feature

--- morningDjangoProject/settings.py
+++ morningDjangoProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'morningDjangoProject',
+    'django_daraja',
 ]
 
 MIDDLEWARE = [
--- morningDjangoProject/views.py
+++ morningDjangoProject/views.py
@@ -72,3 +72,15 @@ def update_product(request, id):
             messages.success(request, 'Product updated sucessfully')
             return redirect('products')
         return render(request, 'update-product.html', {'product': product})
+
+@login_required
+def payment(request, id):
+    # select the product being paid
+    product = Product.objects.get(id=id)
+    # Check if the form being submitted has a post method
+    if request.method == "POST":
+        phone_number = request.POST.get('nambari')
+        amount = request.POST.get('bei')
+        # Proceed with the payment by launching mpesa STK
+
+    return render(request, 'payment.html', {'product':product})
